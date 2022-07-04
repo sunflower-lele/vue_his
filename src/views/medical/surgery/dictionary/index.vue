19624a15fb30dd14eba5397f476cb25fe12d0ce7
nyakori <935419854@qq.com>
2022-07-04 18:23:36 +0800

    fix(view): 设置表样式

--- src/views/medical/surgery/dictionary/index.vue
+++ src/views/medical/surgery/dictionary/index.vue
@@ -1,9 +1,27 @@
 <template>
   <el-card class="card">
-    <el-table :data="tableData" height="750px" border>
-      <el-table-column prop="icdCode" label="ICD-9编码" width="180" />
+    <el-table
+      :data="tableData"
+      max-height="750px"
+      stripe
+      border
+      highlight-current-row
+    >
+      <el-table-column prop="icdCode" label="ICD-9编码" width="180" sortable />
       <el-table-column prop="name" label="手术名称" width="540" />
-      <el-table-column prop="level" label="手术分级" width="180" />
+      <el-table-column
+        prop="level"
+        label="手术分级"
+        width="180"
+        sortable
+        :filters="[
+          { text: '一级', value: '一级' },
+          { text: '二级', value: '二级' },
+          { text: '三级', value: '三级' },
+          { text: '四级', value: '四级' },
+        ]"
+        :filter-method="filterLevel"
+      />
       <el-table-column prop="teleprompter" label="提词器" />
     </el-table>
   </el-card>
@@ -29,6 +47,11 @@ export default {
       const { data } = Response
       this.tableData = data
     })
+  },
+  methods: {
+    filterLevel(value, row) {
+      return row.level === value
+    }
   }
 }
 </script>
