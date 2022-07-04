14305b165b34bf0f1bfbed8caaa67bf049007e39
nyakori <935419854@qq.com>
2022-07-04 11:15:39 +0800

    fix(utils): 修改token续签策略

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import { Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
@@ -19,7 +19,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['token'] = getToken()
+      config.headers['Token'] = getToken()
     }
     return config
   },
@@ -56,21 +56,15 @@ service.interceptors.response.use(
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 1) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
+        store.dispatch('user/resetToken').then(() => {
+          location.reload()
         })
       }
       return Promise.reject(new Error(res.message || 'Error'))
     } else {
-      // 响应成功后，若响应中含有newToken字段，则刷新token
-      if (res.newToken !== undefined) {
-        store.dispatch('user/setToken', res.newToken)
+      const headers = response.headers
+      if (headers['Token'.toLowerCase()] !== undefined) {
+        store.dispatch('user/setToken', headers['Token'.toLowerCase()])
       }
       return res
     }
